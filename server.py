7aa79633711aecf877b6d8d89298628b04352a36
fawazalrasyid <alrasyid.fawaz16@gmail.com>
2023-05-28 21:10:55 +0700

    fix: http request handler

--- server.py
+++ server.py
@@ -1,47 +1,57 @@
-#Import packages
-import threading
-from socket import *
-import sys
+import socket  # Mengimpor modul socket untuk komunikasi jaringan
+import os  # Mengimpor modul os untuk operasi sistem
 
-#Set server address
-serverPort = 8081
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+def get_file(path):
+    with open(path, 'rb') as file:
+        return file.read()  # Membaca dan mengembalikan isi file yang diberikan oleh path
 
-def handle_request(connectionSocket):
-    try:
-        message = connectionSocket.recv(1024).decode()
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = f.read()
-        
-        #Send one HTTP header line into socket
-        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-        
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-            
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
-        
-    except IOError:
-        #Send response message forfile not found
-        connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
-        connectionSocket.send('<html><head></head><body><h1>404 Not Found</h1></body></html>'.encode())
-        
-        #Close client socket
-        connectionSocket.close()
+def handle_http_request(request):
+    method, path, _ = request.split('\n')[0].split()  # Memisahkan metode, path, dan protokol dari baris request
+    if path == "/" or path == "":
+        path = "index.html"  # Jika path adalah "/" atau string kosong, ubah path menjadi "index.html"
 
-while True:
-    #Establish the connection
-    print('Server ready to serve...')
-    connectionSocket, addr =  serverSocket.accept()
-    
-    #Create a new thread for each client
-    client_thread = threading.Thread(target=handle_request, args=(connectionSocket,))
-    client_thread.start()
+    if method == "GET":
+        if os.path.exists(path):
+            content = get_file(path)  # Membaca konten file jika path yang diminta ada
+            response_header = "HTTP/1.1 200 OK\r\n"  # Header respons dengan status 200 OK
+            response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\n\r\n".encode('utf-8') + content  # Respons dengan status 200 OK dan konten file
+        else:
+            content = get_file("404.html")  # Jika path tidak ditemukan, gunakan file "404.html"
+            response_header = "HTTP/1.1 404 Not Found"  # Header respons dengan status 404 Not Found
+            response = f"HTTP/1.1 404 Not Found\r\n\r\n".encode('utf-8') + content  # Respons dengan status 404 Not Found dan konten file
+    else:
+        content = get_file("405.html")  # Jika metode bukan GET, gunakan file "405.html"
+        response_header = "HTTP/1.1 405 Method Not Allowed"  # Header respons dengan status 405 Method Not Allowed
+        response = f"HTTP/1.1 405 Method Not Allowed\r\n\r\n".encode('utf-8') + content  # Respons dengan status 405 Method Not Allowed dan konten file
 
-serverSocket.close()
-sys.exit() #Terminate the program after sending the corresponding data
+    return response_header, response  # Mengembalikan header respons dan respons
+
+def start_web_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Membuat soket TCP
+    server_address = ('localhost', 8080)  # Alamat dan port server
+    server_socket.bind(server_address)  # Bind soket ke alamat dan port tertentu
+    server_socket.listen(1)  # Listen koneksi masuk dengan antrian sebanyak 1
+    print('Web server telah dimulai di {}:{}'.format(server_address[0], server_address[1]))  # Menampilkan pesan server telah dimulai
+
+    while True:
+        client_socket, client_address = server_socket.accept()  # Menerima koneksi dari klien
+        print('Menerima koneksi dari: {}'.format(client_address))  # Menampilkan pesan koneksi diterima
+
+        request = client_socket.recv(1024).decode('utf-8')  # Menerima permintaan dari klien
+
+        response_header, response = handle_http_request(request)  # Menangani permintaan dan mendapatkan respons
+
+        method, req, protocol = request.split('\n')[0].split()  # Memisahkan metode, permintaan, dan protokol dari permintaan
+        filepath = os.path.join(os.getcwd(), request.split()[1][1:])  # Menggabungkan direktori kerja saat ini dengan path file yang diminta
+        print('Method   : {}'.format(method))  # Menampilkan metode permintaan
+        print('Request  : {}'.format(req))  # Menampilkan permintaan
+        print('Path     : {}'.format(filepath))  # Menampilkan path file yang diminta
+        print('Protocol : {}'.format(protocol))  # Menampilkan protokol permintaan
+        print('Respone  : {}'.format(response_header))  # Menampilkan header respons
+        print("\n-------------------------------------------------\n")
+
+        client_socket.sendall(response)  # Mengirim respons ke klien
+        client_socket.close()  # Menutup koneksi dengan klien
+
+if __name__ == '__main__':
+    start_web_server()  # Memulai web server
